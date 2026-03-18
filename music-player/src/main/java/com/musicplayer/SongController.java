9ac575f7cfce4399022edfcf8f23fedb86eacbf0
Abigail Burnham <burnhaar@miamioh.edu>
2026-03-18 13:24:30 -0400

    Update SongController with service layer and new endpoints

--- music-player/src/main/java/com/musicplayer/SongController.java
+++ music-player/src/main/java/com/musicplayer/SongController.java
@@ -2,21 +2,24 @@ package com.musicplayer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/songs")
+@CrossOrigin(origins = "*")
 public class SongController {
 
     @Autowired
-    private SongRepository songRepository;
+    private SongService songService;
 
     // Get all songs
     @GetMapping
     public List<Song> getAllSongs() {
-        return songRepository.findAll();
+        return songService.getAllSongs();
     }
 
     // Get songs by artist
@@ -42,4 +45,31 @@ public class SongController {
     public Song addSong(@RequestBody Song newSong) {
         return songRepository.save(newSong);
     }
+
+    // Get by ID 
+@GetMapping("/{id}")
+public ResponseEntity<Song> getSongById(@PathVariable String id) {
+    return songService.getSongById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+
+// Get by title
+@GetMapping("/title/{title}")
+public ArrayList<Song> getByTitle(@PathVariable String title) {
+    return songService.getSongsByTitle(title);
+}
+
+// Update a song
+@PutMapping("/{id}")
+public ResponseEntity<Song> updateSong(@PathVariable String id, @RequestBody Song updatedSong) {
+    return ResponseEntity.ok(songService.updateSong(id, updatedSong));
+}
+
+// Delete a song
+@DeleteMapping("/{id}")
+public String deleteSong(@PathVariable String id) {
+    return songService.deleteSong(id);
+}
+
+
 }
