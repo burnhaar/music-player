af65e0fb3fd502b14b85441924af64b17139b860
Abigail Burnham <burnhaar@miamioh.edu>
2026-03-19 20:39:52 -0400

    Fix MongoDB URI with appName parameter

--- music-player/src/main/java/com/musicplayer/music_player/MusicPlayerApplication.java
+++ music-player/src/main/java/com/musicplayer/music_player/MusicPlayerApplication.java
@@ -7,15 +7,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication(scanBasePackages = {"com.musicplayer", "com.musicplayer.music_player", "com.musicapp.musicapp"})
 @EnableMongoRepositories(basePackages = "com.musicplayer")
 public class MusicPlayerApplication {
-
     public static void main(String[] args) {
-        String mongoUri = System.getenv("MONGO_URI");
-        if (mongoUri != null) {
-            System.setProperty("spring.data.mongodb.uri", mongoUri);
-        } else {
-            System.setProperty("spring.data.mongodb.uri", 
-                "mongodb+srv://moorege2:25foot23@cluster0.xuq2e43.mongodb.net/musicdb?retryWrites=true&w=majority");
-        }
         SpringApplication.run(MusicPlayerApplication.class, args);
     }
 }
